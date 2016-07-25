3d972c302134c543fae8938d8bdbfbf3a0c270c6
Ajey Gore <ajeygore@gmail.com>
2016-07-25 20:40:38 +0700

    Added API Tokens

--- libnss_http/nss_http-group.c
+++ libnss_http/nss_http-group.c
@@ -78,7 +78,7 @@ _nss_http_setgrent_locked(int stayopen)
     char host_name[255];
     char token[255];
     get_config_host(host_name, token);
-    snprintf(url, 512, "%s/group", host_name);
+    snprintf(url, 512, "%s/group?token=%s", host_name, token);
 
     char *response = nss_http_request(url);
     if (!response) {
@@ -198,7 +198,7 @@ _nss_http_getgrgid_r_locked(gid_t gid, struct group *result, char *buffer, size_
     char token[255];
     get_config_host(host_name, token);
 
-    snprintf(url, 512, "%s/group?gid=%d", host_name, gid);
+    snprintf(url, 512, "%s/group?gid=%d&token=%s", host_name, gid, token);
 
     char *response = nss_http_request(url);
     if (!response) {
@@ -255,7 +255,7 @@ _nss_http_getgrnam_r_locked(const char *name, struct group *result, char *buffer
     char token[255];
     get_config_host(host_name, token);
 
-    snprintf(url, 512, "%s/group?name=%s", host_name, name);
+    snprintf(url, 512, "%s/group?name=%s&token=%s", host_name, name, token);
 
     char *response = nss_http_request(url);
     if (!response) {
